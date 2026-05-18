cf196f56943ff59379c100222c15fc914e58d077
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-05-19 00:11:26 +0530

    Time: 0 ms (100%), Space: 15.5 MB (50.27%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -1,44 +1,15 @@
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-int crossSum(int* nums, int left, int mid, int right) {
-
-    int sum = 0;
-    int leftMax = -100000;
-
-    for(int i = mid; i >= left; i--) {
-        sum += nums[i];
-        if(sum > leftMax)
-            leftMax = sum;
-    }
-
-    sum = 0;
-    int rightMax = -100000;
-
-    for(int i = mid + 1; i <= right; i++) {
-        sum += nums[i];
-        if(sum > rightMax)
-            rightMax = sum;
-    }
-
-    return leftMax + rightMax;
-}
-
-int solve(int* nums, int left, int right) {
-
-    if(left == right)
-        return nums[left];
-
-    int mid = (left + right) / 2;
-
-    int leftSum = solve(nums, left, mid);
-    int rightSum = solve(nums, mid + 1, right);
-    int middleSum = crossSum(nums, left, mid, right);
-
-    return max(max(leftSum, rightSum), middleSum);
-}
-
 int maxSubArray(int* nums, int numsSize) {
-    return solve(nums, 0, numsSize - 1);
+    int current=nums[0];
+    int max=nums[0];
+    for(int i=1;i<numsSize;i++){
+        if(current+nums[i]>nums[i]){
+            current+=nums[i];
+        }else{
+            current=nums[i];
+        }
+        if(current > max){
+            max=current;
+        }
+    }
+    return max;
 }
