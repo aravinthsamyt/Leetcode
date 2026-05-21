96847d196b374437a19ded8ce970c21ea53f1694
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-05-21 21:27:22 +0530

    Time: 0 ms (100%), Space: 9 MB (20%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.c
@@ -0,0 +1,25 @@
+char* maximumOddBinaryNumber(char* s) {
+    char arr[10],arr1[10];
+    int i=0,count1=0,count0=0;
+    int l=strlen(s);
+    while(s[i] != '\0'){
+        if(s[i]=='0'){
+           count0++;
+        }else{
+           count1++;
+        }
+        i++;
+    }
+    i=0;
+    char *result = (char*)malloc((l+1)*sizeof(char));
+    memset(result,'0',l);
+    result[l]='\0';
+    while(i<count1-1){
+        result[i++]='1';
+    }
+    while(i<(l-1)){
+        result[i++]='0';
+    }
+    result[i]='1';
+    return result; 
+}
