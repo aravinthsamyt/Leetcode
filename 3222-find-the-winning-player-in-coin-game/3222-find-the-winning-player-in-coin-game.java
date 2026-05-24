20c3541bbcd75f5e1535fe9d6f814c96fb740b17
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-05-24 14:11:19 +0530

    Time: 1 ms (79.17%), Space: 43.6 MB (14.32%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String winningPlayer(int x, int y) {
+        if(y<4) return "Bob";
+        int moves=x<(y/4)?x:(y/4);
+        if(moves%2 == 0){
+            return "Bob";
+        }else{
+            return "Alice";
+        }
+        
+    }
+}
