6c2bd3d3faa9caa98ee4c5551362874de29d6a31
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-05-25 23:25:12 +0530

    Time: 0 ms (100%), Space: 42.2 MB (91.69%) - LeetHub

--- 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.java
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.java
@@ -1,20 +1,15 @@
 class Solution {
     public String categorizeBox(int length, int width, int height, int mass) {
-
-        long volume = (long) length * width * height;
-
-        boolean bulky =
-                volume >= 1000000000L ||
-                length >= 10000 ||
-                width >= 10000 ||
-                height >= 10000;
-
-        boolean heavy = mass >= 100;
-
-        if (bulky && heavy) return "Both";
-        if (bulky) return "Bulky";
-        if (heavy) return "Heavy";
-
-        return "Neither";
+        long volume=(long)length*width*height;
+        if(volume>=1000000000L || (length>=10000 || width>=10000 || height>=10000)){
+            if(mass>=100){
+                return "Both";
+            }
+            return "Bulky";
+        }else if(mass>=100){
+            return "Heavy";
+        }else{
+            return "Neither";
+        }
     }
 }
