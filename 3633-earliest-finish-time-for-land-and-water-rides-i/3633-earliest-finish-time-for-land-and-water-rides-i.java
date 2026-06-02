56f052f222c6817d050db3dc724c7702992d392a
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-06-02 23:49:13 +0530

    Time: 3 ms (66.67%), Space: 47 MB (23.41%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,26 +1,19 @@
 class Solution {
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
-                                  int[] waterStartTime, int[] waterDuration) {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int earliestTime=Integer.MAX_VALUE,landStart=0,waterStart=0,waterFinish,landFinish;
+        for(int i=0;i<landStartTime.length;i++){
+            for(int j=0;j<waterStartTime.length;j++){
+                landStart=landStartTime[i]+landDuration[i];
+                waterStart=Math.max(landStart,waterStartTime[j]);
+                waterFinish=waterStart+waterDuration[j];
 
-        int earliestTime = Integer.MAX_VALUE;
+                waterStart=waterStartTime[j]+waterDuration[j];
+                landStart=Math.max(waterStart,landStartTime[i]);
+                landFinish=landStart+landDuration[i];
 
-        for (int i = 0; i < landStartTime.length; i++) {
-            for (int j = 0; j < waterStartTime.length; j++) {
-
-                // Land -> Water
-                int landFinish = landStartTime[i] + landDuration[i];
-                int waterStart = Math.max(landFinish, waterStartTime[j]);
-                int finish1 = waterStart + waterDuration[j];
-
-                // Water -> Land
-                int waterFinish = waterStartTime[j] + waterDuration[j];
-                int landStart = Math.max(waterFinish, landStartTime[i]);
-                int finish2 = landStart + landDuration[i];
-
-                earliestTime = Math.min(earliestTime, Math.min(finish1, finish2));
+                earliestTime=Math.min(earliestTime,Math.min(landFinish,waterFinish));
             }
         }
-
         return earliestTime;
     }
 }
