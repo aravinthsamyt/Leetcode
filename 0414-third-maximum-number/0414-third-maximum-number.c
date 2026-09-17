d9f0165a90511be66f9ae65e35a99fd159312c22
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-09-17 07:10:46 +0530

    Time: 0 ms (100%), Space: 9.1 MB (44.89%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.c
+++ 0414-third-maximum-number/0414-third-maximum-number.c
@@ -1,22 +1,13 @@
 int thirdMax(int* nums, int numsSize) {
-    int n = numsSize;
-    long max1 = LONG_MIN, max2 = LONG_MIN, max3 = LONG_MIN;
-
-    for (int i = 0; i < n; i++) {
-        if (max1 < nums[i]) {
-            max3 = max2;
-            max2 = max1;
-            max1 = nums[i];
-        } 
-        else if (max2 < nums[i] && nums[i] != max1) {
-            max3 = max2;
-            max2 = nums[i];
-        } 
-        else if (max3 < nums[i] && nums[i] != max2 && nums[i] != max1) {
-                max3 = nums[i];
+    long int m1=LONG_MIN,m2=m1,m3=m2;
+    for( ;numsSize-- ; nums++){
+        if(m1 < *nums){
+            m3=m2,m2=m1,m1=*nums;
+        }else if(m2 < *nums && *nums != m1){
+            m3=m2,m2=*nums;
+        }else if(m3 < *nums && *nums != m2 && *nums != m1){
+            m3=*nums;
         }
     }
-
-    return max3 == LONG_MIN ? max1 : max3;
-
+    return m3==LONG_MIN?m1:m3;
 }
