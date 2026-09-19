5af6ec7509fef6c66362dd155e7e1d7955a587fd
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-09-19 18:53:32 +0530

    Time: 0 ms (100%), Space: 8.9 MB (0.87%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.c
@@ -0,0 +1,9 @@
+char* toLowerCase(char* s) {
+    int len = strlen(s);
+    for(int i=0;i<len;i++){
+        if(s[i]>= 65 && s[i]<=90){
+            s[i] = s[i]+32;
+        }
+    }
+    return s;
+}
