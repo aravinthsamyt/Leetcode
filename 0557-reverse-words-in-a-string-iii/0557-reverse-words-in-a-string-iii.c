c32fa14d963a2131097c8f5b27095fa78deaa4fa
AravinthSamy T <aravinthtamil2005@gmail.com>
2026-09-19 10:55:13 +0530

    Time: 3 ms (20.15%), Space: 9.9 MB (58.42%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.c
@@ -0,0 +1,21 @@
+char* reverseWords(char* s) {
+    int length = strlen(s),n=0;
+    for(int i=0;i<length;i++){
+        if(s[i]!=' '){ 
+            n++;
+        }
+        if(s[i+1] == ' ' || s[i+1]=='\0'){
+           int x = (i+1)-n,y=i;
+           while(x<y){
+                int temp = s[x];
+                s[x] = s[y];
+                s[y] = temp;
+                x++;
+                y--;
+           }
+           n=0;
+        }
+    }
+    return s;
+
+}
